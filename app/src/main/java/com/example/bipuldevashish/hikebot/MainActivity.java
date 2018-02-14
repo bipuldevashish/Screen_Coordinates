e0128580a4fd58b9a92e9df06e5ec72a46ccc149
Bipul Devashish <aryanbipul@gmail.com>
2018-02-14 23:27:42 +0530

    google ads added..!

--- app/src/main/java/com/example/bipuldevashish/hikebot/MainActivity.java
+++ app/src/main/java/com/example/bipuldevashish/hikebot/MainActivity.java
@@ -9,8 +9,13 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.TextView;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class MainActivity extends AppCompatActivity {
 
+    private AdView mAdview;
     TextView textView;
 
     @Override
@@ -21,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        MobileAds.initialize(this,"ca-app-pub-5374797131081714~4879180373");
+        mAdview = (AdView)findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdview.loadAd(adRequest);
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
